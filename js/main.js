9a20ddcda20d22dd9e1b2f6972e1c8caa9b159a2
Debashis-Roy <joyskaku@gmail.com>
2018-09-30 15:04:38 +0600

    Responsive issue

--- js/main.js
+++ js/main.js
@@ -40,11 +40,11 @@ jQuery(document).ready(function ($) {
         $('body').append($mobile_nav);
         $('body').prepend('<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>');
         $('body').append('<div id="mobile-body-overly"></div>');
-        $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
+        $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-angle-down"></i>');
         $(document).on('click', '.menu-has-children i', function (e) {
             $(this).next().toggleClass('menu-item-active');
             $(this).nextAll('ul').eq(0).slideToggle();
-            $(this).toggleClass("fa-chevron-up fa-chevron-down");
+            $(this).toggleClass("fa-angle-up fa-angle-down");
         });
         $(document).on('click', '#mobile-nav-toggle', function (e) {
             $('body').toggleClass('mobile-nav-active');
